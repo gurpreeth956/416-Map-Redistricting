3672aec6353095e23520a1b57a05f417e9c4a31c
gurpreeth956 <gurpreeth956@gmail.com>
2020-11-19 04:45:05 -0500

    Basic scripts and db connection working

--- src/main/java/com/giants/JobHandler.java
+++ src/main/java/com/giants/JobHandler.java
@@ -9,34 +9,94 @@ import org.hibernate.Transaction;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 import javax.transaction.Transactional;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.List;
 
 public class JobHandler {
 
+    private Hashtable<Integer, Job> jobs;
+    private List<Integer> jobsToCheckStatus;
+    private String pennsylvaniaPrecinctData;
+    private String louisianaPrecinctData;
+    private String californiaPrecinctData;
+
+    /**
+     * This method is called when the server starts up. It is used for instantiating
+     * the instance variables and getting jobs and precinct data from the database.
+     */
+    public void initialSetup() {
+        jobs = new Hashtable<Integer, Job>();
+        List<Job> jobList = loadAllJobData();
+        jobsToCheckStatus = new ArrayList<Integer>();
+        for (Job job : jobList) {
+            jobs.put(job.getId(), job);
+            if (job.getJobStatus() != JobStatus.COMPLETED && job.getJobStatus() != JobStatus.CANCELLED) {
+                jobsToCheckStatus.add(job.getId());
+            }
+        }
+
+        // Need a to verify format for Precinct GeoJSON
+        pennsylvaniaPrecinctData = loadPrecinctData(StateAbbreviation.PA);
+        louisianaPrecinctData = loadPrecinctData(StateAbbreviation.LA);
+        californiaPrecinctData = loadPrecinctData(StateAbbreviation.CA);
+    }
+
+    /**
+     * This method is for getting the specified state precinct data.
+     *
+     * @param stateAbbreviation - Abbreviation for state
+     * @return Precinct data as a string
+     */
+    public String getStateData(StateAbbreviation stateAbbreviation) {
+        if (stateAbbreviation == StateAbbreviation.CA) {
+            return californiaPrecinctData;
+        } else if (stateAbbreviation == StateAbbreviation.PA) {
+            return pennsylvaniaPrecinctData;
+        } else {
+            return louisianaPrecinctData;
+        }
+    }
+
+    /**
+     * This method is for creating a job. It decides where to run the job and then adds
+     * the job to the jobs table and list of jobs to check if it will run on SeaWulf. It
+     * also adds the job to the database.
+     *
+     * @param stateName - user specified state for the jb=ob
+     * @param userCompactness - user specified compactness
+     * @param populationDifferenceLimit - user specified population percent difference
+     * @param userEthnicities - user specified ethnicities
+     * @param numberOfMaps - user specified number of maps
+     * @return The created job object
+     */
     public Job createJob(StateAbbreviation stateName, int userCompactness, double populationDifferenceLimit,
                          List<RaceEthnicity> userEthnicities, int numberOfMaps) {
         Job job = new Job(stateName, userCompactness, populationDifferenceLimit, numberOfMaps);
 
+        // GET RID OF MAGIC NUMBERRRRRRRR
         if (numberOfMaps > 100) {
-            job.setOnSeaWulf(0);
+
+            /*
+             Temporary command for testing seawulf (slurm runs the multiproc file which
+             is also stores in SeaWulf at ~/testing
+             */
+            String command = "cat src/main/resources/seawulf.slurm | ssh gurpreetsing@login.seawulf.stonybrook.edu " +
+                    "'source /etc/profile.d/modules.sh; module load slurm; module load anaconda/2; module load " +
+                    "mvapich2/gcc/64/2.2rc1; cd ~/testing; sbatch'";
+            String processOutput = createScript(command);
+            if (!processOutput.contains("Submitted batch job")) return null;
+            job.setOnSeaWulf(Integer.parseInt(processOutput.split("\\s+")[3]));
             job.setJobStatus(JobStatus.WAITING);
-            int seaWulfId = job.executeSeaWulfJob();
-            if (seaWulfId == -1) {
-                // Error sending job to seawulf
-            }
-            job.setOnSeaWulf(seaWulfId);
-            // CHANGE SEAWULF TO JOB ID IN DATABASE
+            System.out.println(job.getOnSeaWulf());
         } else {
             job.setOnSeaWulf(-1);
             job.setJobStatus(JobStatus.RUNNING);
             job.executeLocalJob();
         }
 
-        // CREATE ETHNICITY OBJECTS THEN PERSIST
-
-        // Now add job to database
         EntityManager em = JPAUtility.getEntityManager();
 //        EntityTransaction txn = em.getTransaction();
 //        if (txn.isActive()) {
@@ -47,19 +107,15 @@ public class JobHandler {
 //            }
 //        }
         try {
+            // Add new job to the db
             em.getTransaction().begin();
             em.persist(job);
             System.out.println(job.getId());
-
             // Create and add all the ethnicities
             List<Ethnicity> jobEthnicities = new ArrayList<>();
             for (RaceEthnicity userEthnicity : userEthnicities) {
                 jobEthnicities.add(new Ethnicity(job.getId(), userEthnicity));
             }
-//            for (Ethnicity jobEthnicity : jobEthnicities) {
-//                em.persist(jobEthnicity);
-//            }
-
             job.setEthnicities(jobEthnicities);
             em.getTransaction().commit();
         } catch (Exception e) {
@@ -67,71 +123,78 @@ public class JobHandler {
             System.out.println(e.getMessage());
             em.getTransaction().rollback();
             return null;
-        } finally {
-//            em.close();
         }
-
-        // Testing find method, works great!
-//        Job job1 = em.find(Job.class, job.getId());
-//        System.out.println(job1.getId() + " " + job1.getAbbreviation());
-//        for (Ethnicity e : job1.getEthnicities()) {
-//            System.out.print(e.getJob().getId() + " " + e.getPrimaryKey().getEthnicity() + " " + e.getPrimaryKey().getId());
-//        }
-
+        jobs.put(job.getId(), job);
+        if (job.getOnSeaWulf() != -1) jobsToCheckStatus.add(job.getId());
         return job;
     }
 
+    /**
+     * This method is for cancelling a job. It updates the job status in the database
+     * and in the jobs table and in the jobsToCheckStatus list.
+     *
+     * @param jobId - Id of specified job
+     * @return Boolean for success
+     */
     public boolean cancelJobData(int jobId) {
-        // Submit slurm to SeaWulf to cancel
-        // For now pretend isCancelled is returned from seawulf (if running then return false)
-        boolean isCancelled = true;
-
-        // Based on isCancelled, use entity manager to update job info in db
-        if (isCancelled) {
-            EntityManager em = JPAUtility.getEntityManager();
-            try {
-                // Start of transaction
-                em.getTransaction().begin();
-                // Change database job status to cancelled
-                Query q = em.createQuery("UPDATE Job SET jobStatus = :jobStatus WHERE id = :id");
-                q.setParameter("id", jobId);
-                q.setParameter("jobStatus", JobStatus.CANCELLED);
-                q.executeUpdate();
-                em.getTransaction().commit();
-            } catch (Exception e) {
-                // Return some kind of error here
-                System.out.println(e.getMessage());
-                em.getTransaction().rollback();
-                return false;
-            } finally {
-
-            }
-        }
-
-        return isCancelled;
+        Job job = jobs.get(jobId);
+//        if (job == null || job.getOnSeaWulf() == -1) return false;
+        String command = String.format("ssh gurpreetsing@login.seawulf.stonybrook.edu " +
+                "'source /etc/profile.d/modules.sh; module load slurm; scancel %d'", job.getOnSeaWulf());
+        String processOutput = createScript(command);
+        jobs.replace(jobId, job);
+        jobsToCheckStatus.remove(new Integer(jobId));
+        return changeJobStatus(jobId, JobStatus.CANCELLED);
     }
 
+    /**
+     * This method is for deleting a job. It removes the job in the database and in the
+     * jobs table and in the jobsToCheckStatus list. It cancels the job if it needs to
+     * before deleting.
+     *
+     * @param jobId - Id of specified job
+     * @return Boolean for success
+     */
     public boolean deleteJobData(int jobId) {
+        Job job = jobs.get(jobId);
+        // If job is running/waiting cancel job
+        if (job.getJobStatus() == JobStatus.RUNNING || job.getJobStatus() == JobStatus.WAITING) {
+            boolean isCancelled = cancelJobData(jobId);
+            if (!isCancelled) {
+                System.out.println("Issue cancelling job");
+                return false;
+            }
+        }
         EntityManager em = JPAUtility.getEntityManager();
         try {
-            // Start of transaction
-            em.getTransaction().begin();
             // Delete job tuple from database
+            em.getTransaction().begin();
             Query q = em.createQuery("DELETE Job WHERE id = :id");
             q.setParameter("id", jobId);
+//            Job job = em.find(Job.class, jobId);
+//            for (Ethnicity ethnicity : job.getEthnicities()) {
+//                em.remove(ethnicity);
+//            }
+//            em.remove(job);
             q.executeUpdate();
             em.getTransaction().commit();
+            // make sure to remove all instances of job object from server
         } catch (Exception e) {
             // Return some kind of error here
+            System.out.println(e.getMessage());
             em.getTransaction().rollback();
             return false;
-        } finally {
-
         }
-
+        jobs.remove(jobId);
         return true;
     }
 
+    /**
+     * This method is called when the server starts up and retrieves all the jobs
+     * for the database and stores it in the jobs list
+     *
+     * @return List of all the jobs
+     */
     public List<Job> loadAllJobData() {
         List<Job> jobs = new ArrayList<Job>();
 
@@ -147,8 +210,6 @@ public class JobHandler {
             System.out.println(e.getMessage());
             em.getTransaction().rollback();
             return null;
-        } finally {
-
         }
         return jobs;
     }
@@ -170,10 +231,7 @@ public class JobHandler {
             // Return some kind of error here
             em.getTransaction().rollback();
             return null;
-        } finally {
-
         }
-
         // Return
         String geoJson = "";
         for (District district : districts) {
@@ -200,10 +258,7 @@ public class JobHandler {
 
 //            em.getTransaction().rollback();
 //            return null;
-        } finally {
-
         }
-
         String geoJson = "";
         for (Precinct precinct : precincts) {
             geoJson += precinct.getGeoJson();
@@ -211,58 +266,153 @@ public class JobHandler {
         return geoJson;
     }
 
-    public List<Job> getJobStatusSeaWulf(List<Job> jobs) {
+    public List<Job> getJobHistory() {
+        List<Job> jobList = new ArrayList<Job>();
+        for (Integer id : jobs.keySet()) {
+            jobList.add(jobs.get(id));
+        }
+        return jobList;
+    }
+
+    public void getJobStatusSeaWulf() {
         // For each job in jobs that is waiting or running check SeaWulf
+        List<Job> jobsToCheck = new ArrayList<Job>();
+        for (int id : jobsToCheckStatus) {
+            jobsToCheck.add(jobs.get(id));
+        }
         // Pretend status is returned from SeaWulf
         // Iterator to handle sync issues
-        Iterator<Job> jobIterator = jobs.iterator();
+        Iterator<Job> jobIterator = jobsToCheck.iterator();
         while (jobIterator.hasNext()) {
             Job job = jobIterator.next();
-            // Send slurm to check current status
-            String status = "RUNNING";
-            if (status.equals(JobStatus.COMPLETED.toString())) {
-                // Send slurm script to calculate data (avg, extreme, boxwhiskers
-                job.setJobStatus(JobStatus.COMPLETED);
-                String filePath = job.retrieveSeaWulfData();
-                // Methods below here return boolean not sure if check is necessary
-                job.countCounties(filePath);
-                job.generateJsonFile(filePath);
-                job.generateAvgExtDistrictingPlan(filePath);
-                job.generateBoxWhiskers(filePath);
-
-                // Update the database
-                EntityManager em = JPAUtility.getEntityManager();
-                try {
-                    // Start of transaction
-                    em.getTransaction().begin();
-                    // Change database job status to cancelled
-                    Query q = em.createQuery("UPDATE Jobs SET status = :status, averageStateId = :averageStateId," +
-                            "extremeStateId = :extremeStateId WHERE id = :id");
-                    q.setParameter("id", job.getId());
-                    q.setParameter("averageStateId", job.getAverageStateId());
-                    q.setParameter("extremeStateId", job.getExtremeStateId());
-                    q.setParameter("status", "cancelled");
-                    q.executeUpdate();
-
-                    // UPDATE BOX WHISKER TABLE HEREEE
-
-                    em.getTransaction().commit();
-                } catch (Exception e) {
-                    // Return some kind of error here
-                    em.getTransaction().rollback();
-                    return null;
-                } finally {
-
+            System.out.println(job.getId());
+            if (job.getJobStatus() == JobStatus.WAITING) {
+                // Check if status changed from waiting
+                String command = String.format("ssh gurpreetsing@login.seawulf.stonybrook.edu " +
+                        "'source /etc/profile.d/modules.sh; module load slurm; squeue -j %d'", job.getOnSeaWulf());
+                String processOutput = createScript(command);
+                if (!processOutput.contains("PD")) {
+                    changeJobStatus(job.getId(), JobStatus.RUNNING);
                 }
-            } else if (!job.getJobStatus().equals(JobStatus.valueOf(status))) {
-                job.setJobStatus(JobStatus.valueOf(status));
-                // Merge in Entity Manager
-            } else {
-                // Only jobs with changed status will stay in list of jobs to return
-                jobIterator.remove();
+            }
+            if (job.getJobStatus() == JobStatus.RUNNING) {
+                // Check if status change from running
+                String command = String.format("ssh gurpreetsing@login.seawulf.stonybrook.edu " +
+                        "'source /etc/profile.d/modules.sh; module load slurm; sacct -Xj %d'", job.getOnSeaWulf());
+                String processOutput = createScript(command);
+                if (processOutput != null) {
+                    changeJobStatus(job.getId(), JobStatus.COMPLETED);
+                    // Send slurm script to calculate data (avg, extreme, boxwhiskers
+                    String filePath = job.retrieveSeaWulfData();
+                    // Methods below here return boolean not sure if check is necessary
+                    job.countCounties(filePath);
+                    job.generateJsonFile(filePath);
+                    job.generateAvgExtDistrictingPlan(filePath);
+                    job.generateBoxWhiskers(filePath);
+
+                    // Update the database
+//                    EntityManager em = JPAUtility.getEntityManager();
+//                    try {
+//                        // Change database job status to completed
+//                        em.getTransaction().begin();
+//                        Query q = em.createQuery("UPDATE Jobs SET status = :status, averageStateId = :averageStateId," +
+//                                "extremeStateId = :extremeStateId WHERE id = :id");
+//                        q.setParameter("id", job.getId());
+//                        q.setParameter("averageStateId", job.getAverageStateId());
+//                        q.setParameter("extremeStateId", job.getExtremeStateId());
+//                        q.setParameter("jobStatus", job.getJobStatus());
+//                        q.executeUpdate();
+//
+//                        // UPDATE BOX WHISKER TABLE HEREEE
+//
+//                        em.getTransaction().commit();
+//                    } catch (Exception e) {
+//                        // Return some kind of error here
+//                        em.getTransaction().rollback();
+//                        return null;
+//                    }
+                    // Remove job from jobs to check since it is completed
+                    jobIterator.remove();
+                }
             }
         }
+        for (Job job : jobsToCheck) {
+            jobs.replace(job.getId(), job);
+            if (job.getJobStatus() == JobStatus.COMPLETED) {
+                jobsToCheckStatus.remove(new Integer(job.getId()));
+            }
+        }
+    }
 
-        return jobs;
+    /**
+     * This method changes the job status of a job
+     *
+     * @param jobId - Id of the job
+     * @param jobStatus - Status to change job to
+     * @return Boolean for success
+     */
+    private boolean changeJobStatus(int jobId, JobStatus jobStatus) {
+        Job job = jobs.get(jobId);
+        EntityManager em = JPAUtility.getEntityManager();
+        try {
+            // Change job status
+            em.getTransaction().begin();
+            job.setJobStatus(jobStatus);
+            em.getTransaction().commit();
+            // make sure to remove all instances of job object from server
+        } catch (Exception e) {
+            // Return some kind of error here
+            em.getTransaction().rollback();
+            System.out.println(e.getMessage());
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * This method creates a script in a temporary file and runs the script in a process
+     *
+     * @param cmd - String of the command
+     * @return The process output
+     */
+    private String createScript(String cmd) {
+        String processOutput = null;
+        try {
+            File tempScript = File.createTempFile("script", null, new File("./src/main/resources/scripts"));
+            FileWriter fw = new FileWriter(tempScript);
+            fw.write(cmd);
+            fw.close();
+            ProcessBuilder pb = new ProcessBuilder("bash", tempScript.toString());
+            pb.redirectErrorStream(true);
+            Process process = pb.start();
+            processOutput = getProcessOutput(process);
+            tempScript.delete();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return processOutput;
+    }
+
+    /**
+     * This method redirects the processes output from SeaWulf and stores in a variables
+     *
+     * @param process - The process
+     * @return String with the process output
+     */
+    private String getProcessOutput(Process process) {
+        String result = null;
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            StringBuilder builder = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                builder.append(line);
+                builder.append(System.getProperty("line.separator")); // Returns OS dependent line separator
+            }
+            result = builder.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 }
--- src/main/java/com/giants/RequestController.java
+++ src/main/java/com/giants/RequestController.java
@@ -11,6 +11,7 @@ import com.giants.enums.StateAbbreviation;
 import com.giants.enums.JobStatus;
 
 import javax.annotation.PostConstruct;
+import java.io.IOException;
 import java.util.*;
 
 @RestController
@@ -20,11 +21,6 @@ public class RequestController {
 
     private JobHandler jobHandler;
     // Switched to hashtable for synchronization issues
-    private Hashtable<Integer, Job> jobs;
-    private List<Integer> jobsToCheckStatus;
-    private String pennsylvaniaPrecinctData;
-    private String louisianaPrecinctData;
-    private String californiaPrecinctData;
 
 
     // Everything we need to do when server starts up goes here
@@ -32,39 +28,19 @@ public class RequestController {
     @PostConstruct
     public void initialSetup() {
         jobHandler = new JobHandler();
-        jobs = new Hashtable<Integer, Job>();
-        List<Job> jobList = jobHandler.loadAllJobData();
-        jobsToCheckStatus = new ArrayList<Integer>();
-        for (Job job : jobList) {
-            jobs.put(job.getId(), job);
-            if (job.getJobStatus() != JobStatus.COMPLETED && job.getJobStatus() != JobStatus.CANCELLED) {
-                jobsToCheckStatus.add(job.getId());
-            }
-        }
-
-        // Need a to verify format for Precinct GeoJSON
-        pennsylvaniaPrecinctData = jobHandler.loadPrecinctData(StateAbbreviation.PA);
-        louisianaPrecinctData = jobHandler.loadPrecinctData(StateAbbreviation.LA);
-        californiaPrecinctData = jobHandler.loadPrecinctData(StateAbbreviation.CA);
+        jobHandler.initialSetup();
     }
 
     // This will return the specified state's precinct data
     @RequestMapping(value = "/getState", method = RequestMethod.POST)
     public String getState(@RequestParam("id") StateAbbreviation stateAbbreviation) {
-        if (stateAbbreviation == StateAbbreviation.CA) {
-            return californiaPrecinctData;
-        } else if (stateAbbreviation == StateAbbreviation.PA) {
-            return pennsylvaniaPrecinctData;
-        } else {
-            return louisianaPrecinctData;
-        }
+        return jobHandler.getStateData(stateAbbreviation);
     }
 
     // This will return the specified state's district data
     @RequestMapping(value = "/getDistricting", method = RequestMethod.POST)
     public String getDistricting(@RequestParam int stateId) {
-        String geoJson = jobHandler.loadDistrictingData(stateId);
-        return geoJson;
+        return jobHandler.loadDistrictingData(stateId);
     }
 
     // This will be called when a the user creates a job
@@ -72,71 +48,34 @@ public class RequestController {
     public int submitJob(@RequestParam StateAbbreviation stateName, @RequestParam int userCompactness, @RequestParam
             double populationDifferenceLimit, @RequestParam List<RaceEthnicity> ethnicities, @RequestParam int numberOfMaps) {
         Job job = jobHandler.createJob(stateName, userCompactness, populationDifferenceLimit, ethnicities, numberOfMaps);
-        jobs.put(job.getId(), job);
-        jobsToCheckStatus.add(job.getId());
-
         return job.getId();
     }
 
     // This will cancel the specified job
     @RequestMapping(value = "/cancelJob", method = RequestMethod.POST)
     public boolean cancelJob(@RequestParam int jobId) {
-        boolean isCancelled = jobHandler.cancelJobData(jobId);
-        if (isCancelled) {
-            Job job = jobs.get(jobId);
-            job.setJobStatus(JobStatus.CANCELLED);
-            jobs.replace(jobId, job);
-            jobsToCheckStatus.remove(new Integer(jobId));
-        }
         // Based on cancellation, a modal should popup with details
-        return isCancelled;
+        return jobHandler.cancelJobData(jobId);
     }
 
-    // This will ret
+    // This will return the list of jobs
     @RequestMapping(value = "/getJobHistory", method = RequestMethod.GET)
     public List<Job> getHistory() {
-        List<Job> jobList = new ArrayList<Job>();
-        for (Integer id : jobs.keySet()) {
-            jobList.add(jobs.get(id));
-        }
-        return jobList;
+        return jobHandler.getJobHistory();
     }
 
     // This will delete the specified job
     @RequestMapping(value = "/deleteJob", method = RequestMethod.POST)
-    public boolean deleteJob(@RequestParam int jobId) {
-        Job job = jobs.get(jobId);
-        // If job is running/waiting cancel job
-        if (job.getJobStatus() == JobStatus.RUNNING || job.getJobStatus() == JobStatus.WAITING) {
-            boolean isCancelled = cancelJob(jobId);
-            if (!isCancelled) {
-                System.out.println("Issue cancelling job");
-            }
-        }
-        boolean isDeleted = jobHandler.deleteJobData(jobId);
-        if (isDeleted) {
-            jobs.remove(jobId);
-        }
-        return isDeleted;
+    public boolean deleteJob(@RequestParam int jobId)  {
+        return jobHandler.deleteJobData(jobId);
     }
 
     // Current scheduled for every 5 seconds
     // fixedDelay is in milliseconds
     // https://www.baeldung.com/spring-scheduled-tasks
     @Scheduled(fixedDelay = 5000)
-    public void checkJobStatus() {
-        List<Job> jobsToCheck = new ArrayList<Job>();
-        for (int id : jobsToCheckStatus) {
-            jobsToCheck.add(jobs.get(id));
-        }
-
-        List<Job> changedJobs = jobHandler.getJobStatusSeaWulf(jobsToCheck);
-        for (Job job : changedJobs) {
-            jobs.replace(job.getId(), job);
-            if (job.getJobStatus() == JobStatus.COMPLETED) {
-                jobsToCheckStatus.remove(job.getId());
-            }
-        }
+    public void checkJobStatus()  {
+        jobHandler.getJobStatusSeaWulf();
     }
 
     // Testing/working post method
